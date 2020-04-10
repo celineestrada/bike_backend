06df672acd7b1b5d0faff6c7e70280e5cefbc93d
Celine Estrada <estrada.ce@husky.neu.edu>
2020-04-10 14:12:30 -0400

    fix view connection

--- frontend/urls.py
+++ frontend/urls.py
@@ -13,15 +13,22 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.urls import path
-from views import acident_point
+from django.urls import path, include
+from . import views
+
 
 urlpatterns = [
     # path('', views.Home.as_view()),
+    path('', views.hello_maps, name='hello_maps')
+]
+
+"""
+    path('admin/', admin.site.urls),
+    path('', include('index.html')),
     path(r'^accident-points/?$', acident_point.AccidentPointList.asView(), name='accident-point-list'),
     path(r'^accident-points/(?P<pk>[0-9]+)/?$', acident_point.AccidentPointDetail.as_view(), name='accident-point'
                                                                                                   '-detail'),
     path(r'^map/P<zoom>[0-9]+)/(?P<x>[0-9]+)/(?P<y>[0-9]+)/?$',
          acident_point.AccidentPointMap.as_view(),
          name='accident-point-map')
-]
+"""
--- root/settings.py
+++ root/settings.py
@@ -51,10 +51,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'root.urls'
 
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
