09b26eb8cf5235a0839a8831a29d67b3a0115658
Celine Estrada <estrada.ce@husky.neu.edu>
2020-04-13 17:19:49 -0400

    map queries saved to database

--- bike/api/admin.py
+++ bike/api/admin.py
@@ -1,28 +1,28 @@
-from django.contrib.gis import admin
-from .models import AccidentPoint
-from .models import Hospital
-from .models import Streetlight
-
-
-class AccidentPointAdmin(admin.OSMGeoAdmin):
-    list_display = ['id', 'latitude', 'longitude']
-    list_search = ['id', 'latitude', 'longitude']
-
-
-admin.site.register(AccidentPoint, AccidentPointAdmin)
-
-
-class HospitalAdmin(admin.OSMGeoAdmin):
-    list_display = ('id', 'name', 'latitude', 'longitude')
-    list_search = ['id', 'name']
-
-
-admin.site.register(Hospital, HospitalAdmin)
-
-
-class SteetlightAdmin(admin.OSMGeoAdmin):
-    list_display = ('id', 'latitude', 'longitude')
-    list_search = ['id', 'latitude', 'longitude']
-
-
-admin.site.register(Streetlight, SteetlightAdmin)
+# from django.contrib.gis import admin
+# from .models import AccidentPoint
+# from .models import Hospital
+# from .models import Streetlight
+#
+#
+# class AccidentPointAdmin(admin.OSMGeoAdmin):
+#     list_display = ['id', 'latitude', 'longitude']
+#     list_search = ['id', 'latitude', 'longitude']
+#
+#
+# admin.site.register(AccidentPoint, AccidentPointAdmin)
+#
+#
+# class HospitalAdmin(admin.OSMGeoAdmin):
+#     list_display = ('id', 'name', 'latitude', 'longitude')
+#     list_search = ['id', 'name']
+#
+#
+# admin.site.register(Hospital, HospitalAdmin)
+#
+#
+# class SteetlightAdmin(admin.OSMGeoAdmin):
+#     list_display = ('id', 'latitude', 'longitude')
+#     list_search = ['id', 'latitude', 'longitude']
+#
+#
+# admin.site.register(Streetlight, SteetlightAdmin)
